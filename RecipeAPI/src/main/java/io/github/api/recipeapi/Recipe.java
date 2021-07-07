764a165c73372f2a34dba40c96aa8e092a704f5e
RANK <81941006+AtSignRANK@users.noreply.github.com>
2021-07-08 08:38:05 +0900

    developing

--- RecipeAPI/src/main/java/io/github/api/recipeapi/Recipe.java
+++ RecipeAPI/src/main/java/io/github/api/recipeapi/Recipe.java
@@ -5,12 +5,18 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ShapedRecipe;
 
 public class Recipe {
+    File file = File("RecipeNamespacedKeys/namespacedkey.yml")
+    FileConfiguration namespace;
+
     public void addRecipe(ShapedRecipe shapedrecipe) {
+        int number = namespace.get("number") + 1
         NamespacedKey namespacedkey = shapedrecipe.getKey();
         Bukkit.addRecipe(shapedrecipe);
+        namespace.set(shapedrecipe.toString(), namespacedkey)
     }
 
-    public void removeRecipe(ShapedRecipe shapedRecipe) {
-        Bukkit.removeRecipe()
+    public void removeRecipe(ShapedRecipe shapedrecipe) {
+        NamespacedKey namespacedkey = namespace.get(shapedrecipe.toString())
+        Bukkit.removeRecipe(namespacedkey)
     }
 }
